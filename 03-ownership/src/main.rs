5ce0b859ffdd308ffcba6220f9901edb543ca2ea
baekcg <doci1803@naver.com>
2022-09-25 16:05:03 +0900

    03. Ownership

--- 03-ownership/src/main.rs
+++ 03-ownership/src/main.rs
@@ -1,3 +1,21 @@
 fn main() {
-    println!("Hello, world!");
+    let s1: String = String::from("hello");
+	let s2: String = s1; // s1 무력화 얕은복사로 포인터를 복사하는게 아니라 걍 다 s1이 s2로 이동함
+
+	println!("{} World !", s2);
+
+	// 깊은 복사라고 생각하면 될듯? 
+	let s1: String = String::from("hello");
+	let s2: String = s1.clone();
+
+	println!("s1 = {}, s2 = {}", s1, s2);
+
+}
+
+fn teakes_ownership(some_string: String) {
+	println!("{}", some_string);
+}
+
+fn makes_copy(some_integer: i32) {
+	println!("{}", some_integer);
 }
